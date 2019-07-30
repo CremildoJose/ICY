a3193c6a7fa83589f96c4ba79aaade9bc51a5ec7
CremildoJose <30451450+CremildoJose@users.noreply.github.com>
2019-07-30 20:00:20 +0200

    Add files via upload

--- db.php
+++ db.php
@@ -1,8 +1,49 @@
-	<?php
-	$host="localhost";
-	$dbuser="root";
-	$dbpwd="";
-	$connect = mysqli_connect($host, $dbuser, $dbpwd) or die ("Nao conectou a base de dados");
-			$select = mysqli_select_db ($connect,"aula-rede-social");
-	?>
-	 
+<?php
+	error_reporting(E_ALL ^ E_DEPRECATED);
+	$connect = mysqli_connect("127.0.0.1","root","") or die("Não foi possível ligar ao servidor...");
+	$db = mysqli_select_db($connect,"aula-rede-social1") or die("Impossível entrar na Base de dados");
+?>
+<html>
+<header>
+	<meta charset="utf-8">
+	<title>Meet new Friends!</title>
+	<style type="text/css">
+		html{
+			-webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+		       -moz-animation: fadein 2s; /* Firefox < 16 */
+		        -ms-animation: fadein 2s; /* Internet Explorer */
+		         -o-animation: fadein 2s; /* Opera < 12.1 */
+		            animation: fadein 2s;
+		}
+
+		@keyframes fadein {
+		    from { opacity: 0; }
+		    to   { opacity: 1; }
+		}
+
+		/* Firefox < 16 */
+		@-moz-keyframes fadein {
+		    from { opacity: 0; }
+		    to   { opacity: 1; }
+		}
+
+		/* Safari, Chrome and Opera > 12.1 */
+		@-webkit-keyframes fadein {
+		    from { opacity: 0; }
+		    to   { opacity: 1; }
+		}
+
+		/* Internet Explorer */
+		@-ms-keyframes fadein {
+		    from { opacity: 0; }
+		    to   { opacity: 1; }
+		}
+
+		/* Opera < 12.1 */
+		@-o-keyframes fadein {
+		    from { opacity: 0; }
+		    to   { opacity: 1; }
+		}}
+	</style>
+</header>
+</html>
--- mobile/login.php
+++ mobile/login.php
@@ -1,11 +1,11 @@
 <?php
-	include("db.php");
+	include("../db.php");
 
 	if (isset($_POST['entrar'])) {
 		$email = $_POST['email'];
 		$pass = $_POST['pass'];
-		$verifica = mysqli_query($connect,"SELECT * FROM users WHERE email = '$email' AND password='$pass'");
-		if (mysqli_num_rows($verifica)<=0) {
+		$verifica = mysql_query("SELECT * FROM users WHERE email = '$email' AND password='$pass'");
+		if (mysql_num_rows($verifica)<=0) {
 			echo "<h3>Palavra-passe ou e-mail errados!</h3>";
 		}else{
 			setcookie("login",$email);
@@ -17,13 +17,14 @@
 <html>
 <head>
 	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
 	<style type="text/css">
 	*{font-family: 'Montserrat', cursive;}
 	img{display: block; margin: auto; margin-top: 20px; width: 200px;}
 	form{text-align: center; margin-top: 20px;}
-	input[type="email"]{border: 1px solid #CCC; width: 250px; height: 25px; padding-left: 10px; border-radius: 3px;}
-	input[type="password"]{border: 1px solid #CCC; width: 250px; height: 25px; padding-left: 10px; margin-top: 10px; border-radius: 3px;}
-	input[type="submit"]{border: none; width: 80px; height: 30px; margin-top: 20px; border-radius: 3px;}
+	input[type="email"]{border: 1px solid #CCC; width: 250px; height: 30px; padding-left: 10px; border-radius: 3px;}
+	input[type="password"]{border: 1px solid #CCC; width: 250px; height: 30px; padding-left: 10px; margin-top: 10px; border-radius: 3px;}
+	input[type="submit"]{border: none; width: 80px; height: 40px; margin-top: 20px; border-radius: 3px; font-size: 14pt;}
 	input[type="submit"]:hover{background-color: #1E90FF; color: #FFF; cursor: pointer;}
 	h2{text-align: center; margin-top: 20px;}
 	h3{text-align: center; color: #1E90FF; margin-top: 15px;}
@@ -31,7 +32,7 @@
 	</style>
 </head>
 <body>
-	<img src="img/logo.png"><br />
+	<img src="../img/logo.png"><br />
 	<h2>Entra na tua conta</h2>
 	<form method="POST">
 		<input type="email" placeholder="Endereço email" name="email"><br />
